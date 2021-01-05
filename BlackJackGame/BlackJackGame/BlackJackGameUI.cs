946d1aaa850032a3dca9e386b4a98ce1010b8b7b
AniCodesIt <73564845+AniCodesIt@users.noreply.github.com>
2021-01-04 21:40:26 -0500

    Add files via upload
    Initial commit. UI and Repo started.

---
+++ BlackJackGame/BlackJackGame/BlackJackGameUI.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackJackGameRepo;
+
+namespace BlackJackGame
+{
+    class BlackJackGameUI
+    {
+        GameRepo repoWindow = new GameRepo();
+        public void RunMenu()
+        {
+            int dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToDealerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToPlayerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToDealerHand(dealtCard);
+            dealtCard = repoWindow.DealACard();
+            repoWindow.AssignToPlayerHand(dealtCard);
+
+            List<int> dealer = repoWindow.DisplayDealerHand();
+            List<int> player = repoWindow.DisplayPlayerHand();
+            bool firstTime = true;
+            foreach (int card in dealer)
+            {
+                if (firstTime == true)
+                {
+                    Console.WriteLine("X");
+                }
+                else
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+            foreach (int card in player)
+            {
+                Console.WriteLine(card);
+            }
+
+
+
+
+            bool isRunning = true;
+            while (isRunning)
+            {
+
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
